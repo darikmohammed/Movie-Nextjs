2134b45d610549a5f72aba4600cd5ad08e7de9eb
biruk-bereka <birukbeletech1212@gmail.com>
2022-09-10 22:37:01 +0300

    fetch characters

--- pages/characters.js
+++ pages/characters.js
@@ -1,11 +1,31 @@
 import React from "react";
+import Link from "next/link";
 
-function characters() {
+export default function characters({moviesCharacters}) {
   return (
-    <div>
-      <h1>Characters</h1>
+    <div className="flex flex-wrap justify-center gap-5">
+      {moviesCharacters.map(movie => (
+        <>
+           <Link href={`/charcaters/${movie.char_id}`}>
+              <div className="mt-3">
+                <img className="h-80 w-80" src={movie.img}/>   
+                <h1>{movie.name}</h1>
+              </div>
+           </Link>  
+        </>
+      ))}
     </div>
   );
 }
 
-export default characters;
+
+export const getStaticProps = async () => {
+  const res = await fetch(`https://www.breakingbadapi.com/api/characters`);
+  const moviesCharacters = await res.json();
+
+  return {
+    props: {
+      moviesCharacters
+    }
+  }
+}
