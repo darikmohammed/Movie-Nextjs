7f150c13fc6169388a3d526d71a247b4483b901b
biruk-bereka <birukbeletech1212@gmail.com>
2022-09-10 21:51:02 +0300

    add nav link

--- components/Nav.js
+++ components/Nav.js
@@ -1,9 +1,28 @@
+import Link from "next/link";
 import React from "react";
 
 function Nav() {
   return (
-    <nav>
-      <h1>Navbar</h1>
+    <nav className="flex justify-between px-6 py-4 bg-neutral-800 text-white ">
+      <h1>Logo</h1>
+    
+      <ul className="hidden md:flex gap-x-3 ">
+        <li className="hover:text-sky-600">
+          <Link href='/'>Home</Link>
+        </li>
+        <li className="hover:text-sky-600">
+          <Link href='/characters'>Characters</Link>
+        </li>
+        <li className="hover:text-sky-600">
+          <Link href='/episodes'>Episodes</Link>
+        </li>
+        <li className="hover:text-sky-600">
+          <Link href='/quotes'>Quotes</Link>
+        </li>
+        <li className="hover:text-sky-600">
+          <Link href='/deaths'>Deaths</Link>
+        </li>
+      </ul>
     </nav>
   );
 }
