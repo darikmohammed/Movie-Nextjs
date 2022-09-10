33d69a63f7dbecfcc3009eb87fbce50fba8944ca
darikmohammed <r.m.darik@gmail.com>
2022-09-10 21:17:13 +0300

    Add navigation menu

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,11 @@
 import Title from "./Title";
+import Nav from "./Nav";
 
 function Layout({ children }) {
   return (
     <div>
       <Title title="Breaking Bad!" />
+      <Nav />
       {children}
     </div>
   );
